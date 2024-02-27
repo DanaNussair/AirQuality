2fa4182cb557b9828e9b1d0a32f2c848f44244f9
Dana Alnusair <dana.nusair88@gmail.com>
2024-02-28 02:15:42 +0300

    4: Adds more tests for AirQuality model

--- src/tests/models/airquality.test.js
+++ src/tests/models/airquality.test.js
@@ -24,4 +24,30 @@ describe('AirQuality Model', () => {
     expect(aq.city).toBe('Paris');
     expect(aq.aqius).toBe(26);
   });
+
+  it('should find added air quality record in database', async () => {
+    const aq = await AirQuality.findOne({ where: { id: '1' } });
+    
+    expect(aq).toBeDefined();
+    expect(aq.city).toBe('Paris');
+  });
+
+  it('should update air quality record', async () => {
+    const aq = await AirQuality.findOne({ where: { id: '1' } });
+    aq.city = 'Amman';
+    await aq.save();
+    
+    const updated_aq = await AirQuality.findOne({ where: { id: '1' } });
+
+    expect(updated_aq).toBeDefined();
+    expect(updated_aq.city).toBe('Amman');
+  });
+
+  it('should delete air quality record successfully', async () => {
+    const aq = await AirQuality.findOne({ where: { id: '1' } });
+    await aq.destroy();
+
+    const deleted_aq = await AirQuality.findOne({ where: { id: '1' } });
+    expect(deleted_aq).toBeNull();
+  });
 });
